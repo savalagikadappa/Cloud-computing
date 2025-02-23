b92a8792e60bc2898ad0d68d620808007037eeb0
kadappa savalagi <kadappasavalagi@gmail.com>
2025-02-23 18:52:14 +0530

    fixed some deployment issues, no major files changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Explicitly set (already the default, just for clarity)
+    sourcemap: true // Optional: generates sourcemaps for debugging
+  }
 })
