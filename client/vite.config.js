bccde709765bc1bd2297fa85813a46addf4f1867
kadappa savalagi <kadappasavalagi@gmail.com>
2025-02-27 22:54:51 +0530

    updated vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist', // Explicitly set (already the default, just for clarity)
     sourcemap: true // Optional: generates sourcemaps for debugging
